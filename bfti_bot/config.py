1d6d5322abfe62d93e039bec19c84ec32b27be25
coma64 <zaton.tristan@gmail.com>
2021-03-19 23:07:18 +0100

    Setup logs

--- bfti_bot/config.py
+++ bfti_bot/config.py
@@ -7,14 +7,16 @@ from dotenv import load_dotenv
 
 class Config(SimpleNamespace):
     """Read only config class loading values from .env and environment"""
+
     def __init__(self):
         self.token = getenv('BOT_TOKEN', '')
         self.prefix = getenv('BOT_PREFIX', '-')
         self.group = getenv('BOT_GROUP', 'yeet')
-        
+
         self.iserv_username = getenv('ISERV_USERNAME', '')
         self.iserv_password = getenv('ISERV_PASSWORD', '')
 
+        self.debug = getenv('DEBUG', 'true') != 'false'
 
 
 load_dotenv()
