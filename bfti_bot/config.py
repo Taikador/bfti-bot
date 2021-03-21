e1a4c7ab6ea0a8d0c31632df1f9ccbdbf6bbcf25
coma64 <zaton.tristan@gmail.com>
2021-03-21 21:56:44 +0100

    Make config system more verbose

--- bfti_bot/config.py
+++ bfti_bot/config.py
@@ -5,22 +5,41 @@ from typing import List
 from dotenv import load_dotenv
 
 
+class ConfigError(Exception):
+    def __init__(self, missing_key: str) -> None:
+        self.missing_key = missing_key
+
+    def __str__(self):
+        return f"Environment variable '{self.missing_key}' doesn't exist"
+
+
 class Config(SimpleNamespace):
     """Read only config class loading values from .env and environment"""
 
     def __init__(self):
-        self.token: str = getenv('BOT_TOKEN', '')
-        self.prefix: str = getenv('BOT_PREFIX', '-')
+        self.token: str = self._getenv_or_throw('BOT_TOKEN', '')
+        self.prefix: str = self._getenv_or_throw('BOT_PREFIX', '-')
         self.moderation_roles: List[int] = [
-            int(role_id) for role_id in getenv('MODERATION_ROLES', 'yeet').split(',')
+            int(role_id)
+            for role_id in self._getenv_or_throw('MODERATION_ROLES').split(',')
         ]
-        self.guild_id: int = int(getenv('GUILD_ID', None))
-        self.channel_id: int = int(getenv('CHANNEL_ID', None))
+        self.guild_id: int = int(self._getenv_or_throw('GUILD_ID'))
+        self.channel_id: int = int(self._getenv_or_throw('CHANNEL_ID'))
 
-        self.iserv_username: str = getenv('ISERV_USERNAME', '')
-        self.iserv_password: str = getenv('ISERV_PASSWORD', '')
+        self.iserv_username: str = self._getenv_or_throw('ISERV_USERNAME')
+        self.iserv_password: str = self._getenv_or_throw('ISERV_PASSWORD')
 
-        self.debug: bool = getenv('DEBUG', 'true') != 'false'
+        self.debug: bool = self._getenv_or_throw('DEBUG', 'true') != 'false'
+
+    def _getenv_or_throw(self, key: str, default: str = None) -> str:
+        value = getenv(key)
+
+        if value == None and default == None:
+            raise ConfigError(key)
+        elif value == None:
+            return default
+        else:
+            return value
 
 
 load_dotenv()
